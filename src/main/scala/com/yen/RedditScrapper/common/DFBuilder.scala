60dcbc5342f8f6c134abf3a297e3be23de35b5f6
yennanliu <f339339@gmail.com>
2021-11-13 15:07:27 +0800

    update

---
+++ src/main/scala/com/yen/RedditScrapper/common/DFBuilder.scala
@@ -0,0 +1,13 @@
+package com.yen.RedditScrapper.common
+
+import org.apache.spark
+import org.apache.spark.sql
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
+
+class DFBuilder {
+  def prepareDF(spark:SparkSession, srcFile:String, dfCols:List[String]): sql.DataFrame = {
+    val df = spark.read.json(srcFile).select(dfCols.map(col): _*)
+    df
+  }
+}
--- src/main/scala/com/yen/RedditScrapper/spark/Processor.scala
+++ src/main/scala/com/yen/RedditScrapper/spark/Processor.scala
@@ -1,27 +1,31 @@
 package com.yen.RedditScrapper.spark
 
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession}
 
-object Processer extends App {
+import com.yen.RedditScrapper.common.DFBuilder
+
+object Processor extends App {
 
   val sc = new SparkContext("local[*]", "helloSpark")
   val spark = SparkSession.builder().appName("Processer").getOrCreate()
 
   import spark.implicits._
+  import org.apache.spark.sql.functions._
 
   /** Load */
 
+  val df_builder = new DFBuilder()
   // comment
   val commentRawPath = "/Users/yennanliu/RedditScrapper/data/raw/comment.json"
-  val commentFilterCols = ("body","id","score","author","author_fullname","parent_id","created_utc")
-  val commentDF = spark.read.json(commentRawPath).select("body","id","score","author","author_fullname","parent_id","created_utc")
+  val commentFilterCols:List[String] = List("body","id","score","author","author_fullname","parent_id","created_utc")
+  val commentDF = df_builder.prepareDF(spark, commentRawPath, commentFilterCols)
   commentDF.show()
 
   // submission
   val submissionRawPath = "/Users/yennanliu/RedditScrapper/data/raw/submission.json"
-  val submissionFilterCols = ("title","selftext","id","upvote_ratio","num_comments","link_flair_text","score","created_utc","author","author_fullname","retrieved_on")
-  val submissionDF = spark.read.json(submissionRawPath)select("title","selftext","id","upvote_ratio","num_comments","link_flair_text","score","created_utc","author","author_fullname","retrieved_on")
+  val submissionFilterCols:List[String] = List("title","selftext","id","upvote_ratio","num_comments","link_flair_text","score","created_utc","author","author_fullname","retrieved_on")
+  val submissionDF = df_builder.prepareDF(spark, submissionRawPath, submissionFilterCols)
   submissionDF.show()
 
   /** Save */
