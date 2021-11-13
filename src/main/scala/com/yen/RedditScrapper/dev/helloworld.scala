e2a02e034c7a2d2fe26e9864cd642c95d2c2014f
yennanliu <f339339@gmail.com>
2021-11-14 07:56:13 +0800

    fix output path, code

--- src/main/scala/com/yen/RedditScrapper/dev/helloworld.scala
+++ src/main/scala/com/yen/RedditScrapper/dev/helloworld.scala
@@ -2,4 +2,5 @@ package com.yen.RedditScrapper.dev
 
 object helloworld extends App {
   println("hello !!!")
+  println(java.time.LocalDate.now)
 }
--- src/main/scala/com/yen/RedditScrapper/spark/Processor.scala
+++ src/main/scala/com/yen/RedditScrapper/spark/Processor.scala
@@ -6,30 +6,45 @@ import org.apache.spark.sql.{SparkSession}
 import com.yen.RedditScrapper.common.DFBuilder
 import com.yen.RedditScrapper.model.Schema
 
-object Processor extends App {
+object Processor{
 
-  val sc = new SparkContext("local[*]", "helloSpark")
-  val spark = SparkSession.builder().appName("Processer").getOrCreate()
+  def main(args: Array[String]): Unit = {
 
-  import spark.implicits._
-  import org.apache.spark.sql.functions._
+//    if (args == 0){
+//      println("argument missing! : --yyyymmdd")
+//    }else{
+//
+//      println("will use default one : yyyymmdd = later")
+//      java.time.LocalDate.now
+//    }
 
-  /** Load */
-  val df_builder = new DFBuilder()
-  val schema = new Schema()
+    val yearMonthDate:String = java.time.LocalDate.now.toString //args(0)
 
-  // comment
-  val commentRawPath = "/Users/yennanliu/RedditScrapper/data/raw/comment.json"
-  val commentDF = df_builder.prepareDF(spark, commentRawPath, schema.commentFilterCols)
-  commentDF.show()
+    println(s"--yyyymmdd = $yearMonthDate")
 
-  // submission
-  val submissionRawPath = "/Users/yennanliu/RedditScrapper/data/raw/submission.json"
-  val submissionDF = df_builder.prepareDF(spark, submissionRawPath, schema.submissionFilterCols)
-  submissionDF.show()
+    val sc = new SparkContext("local[*]", "Processor")
+    val spark = SparkSession.builder().appName("Processor").getOrCreate()
 
-  /** Save */
-  // save to parquet
-  commentDF.write.parquet("/Users/yennanliu/RedditScrapper/data/output/comment")
-  submissionDF.write.parquet("/Users/yennanliu/RedditScrapper/data/output/submission")
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+
+    /** Load */
+    val df_builder = new DFBuilder()
+    val schema = new Schema()
+
+    // comment
+    val commentRawPath = s"/Users/yennanliu/RedditScrapper/data/raw/$yearMonthDate/comment.json"
+    val commentDF = df_builder.prepareDF(spark, commentRawPath, schema.commentFilterCols)
+    commentDF.show()
+
+    // submission
+    val submissionRawPath = s"/Users/yennanliu/RedditScrapper/data/raw/$yearMonthDate/submission.json"
+    val submissionDF = df_builder.prepareDF(spark, submissionRawPath, schema.submissionFilterCols)
+    submissionDF.show()
+
+    /** Save */
+    // save to parquet
+    commentDF.write.parquet(s"/Users/yennanliu/RedditScrapper/data/output/$yearMonthDate/comment")
+    submissionDF.write.parquet(s"/Users/yennanliu/RedditScrapper/data/output/$yearMonthDate/submission")
+  }
 }
