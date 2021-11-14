a10e46b309a072fcfd1bed4faf367f0e4467a159
yennanliu <f339339@gmail.com>
2021-11-14 13:18:06 +0800

    fix path pattern, code

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 yyyymmdd=$1
 echo "yyyymmdd :" $yyyymmdd
 
@@ -8,4 +9,5 @@ python src/main/python/data_loader.py -yyyymmdd $yyyymmdd
 echo '>>> Step 2 : process & sink data (com.yen.RedditScrapper.spark.scala) ...'
 spark-submit \
  --class com.yen.RedditScrapper.spark.Processor \
+ --conf yyyymmdd=$yyyymmdd \
  target/scala-2.11/RedditScrapper-assembly-0.0.1.jar
--- src/main/scala/com/yen/RedditScrapper/spark/Processor.scala
+++ src/main/scala/com/yen/RedditScrapper/spark/Processor.scala
@@ -19,6 +19,8 @@ object Processor{
 //    }
 
     val yearMonthDate:String = java.time.LocalDate.now.toString //args(0)
+    // println("args = " + args.toString)
+    // val yearMonthDate:String = args(0)
 
     println(s"--yyyymmdd = $yearMonthDate")
 
@@ -28,23 +30,23 @@ object Processor{
     import spark.implicits._
     import org.apache.spark.sql.functions._
 
-    /** Load */
     val df_builder = new DFBuilder()
     val schema = new Schema()
 
+    /** Load */
     // comment
-    val commentRawPath = s"/Users/yennanliu/RedditScrapper/data/raw/$yearMonthDate/comment.json"
+    val commentRawPath = s"./data/raw/$yearMonthDate/comment.json"
     val commentDF = df_builder.prepareDF(spark, commentRawPath, schema.commentFilterCols)
     commentDF.show()
 
     // submission
-    val submissionRawPath = s"/Users/yennanliu/RedditScrapper/data/raw/$yearMonthDate/submission.json"
+    val submissionRawPath = s"./data/raw/$yearMonthDate/submission.json"
     val submissionDF = df_builder.prepareDF(spark, submissionRawPath, schema.submissionFilterCols)
     submissionDF.show()
 
     /** Save */
     // save to parquet
-    commentDF.write.parquet(s"/Users/yennanliu/RedditScrapper/data/output/$yearMonthDate/comment")
-    submissionDF.write.parquet(s"/Users/yennanliu/RedditScrapper/data/output/$yearMonthDate/submission")
+    commentDF.write.parquet(s"./data/output/$yearMonthDate/comment")
+    submissionDF.write.parquet(s"./data/output/$yearMonthDate/submission")
   }
 }
