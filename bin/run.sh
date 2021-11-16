7222ea05c8622a7feb61021f58dd87ed72786081
yennj12 <f339339@gmail.com>
2021-11-16 17:38:27 +0800

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -9,6 +9,5 @@ python src/main/python/data_loader.py -yyyymmdd $yyyymmdd
 echo '>>> Step 2 : process & sink data (com.yen.RedditScrapper.spark.scala) ...'
 spark-submit \
  --class com.yen.RedditScrapper.spark.Processor \
- --conf yyyymmdd=$yyyymmdd \
  target/scala-2.11/RedditScrapper-assembly-0.0.1.jar \
- --yyyymmdd=${yyyymmdd}
+ --yyyymmdd=$yyyymmdd
