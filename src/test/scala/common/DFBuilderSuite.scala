e76c59e9f8c860e77cac68a06d5fe386e23896e2
yennanliu <f339339@gmail.com>
2021-11-16 22:57:29 +0800

    add tests

---
+++ src/test/scala/common/DFBuilderSuite.scala
@@ -0,0 +1,23 @@
+package common
+
+import org.scalatest.{FunSuite}
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
+import com.yen.RedditScrapper.common.DFBuilder
+
+class DFBuilderSuite extends FunSuite {
+
+  val sc = new SparkContext("local[*]", "DFBuilderSuite")
+  val spark = SparkSession.builder().appName("DFBuilderSuite").getOrCreate()
+
+  val df_builder = new DFBuilder()
+
+  val mySchema = List("id", "name", "age")
+  val myDF = df_builder.prepareDF(spark, "./data/test/test.json", mySchema)
+
+  myDF.show()
+
+  assert (myDF.columns.toList == mySchema)
+  assert (myDF.count() == 3)
+}
---
+++ src/test/scala/model/SchemaSuite.scala
@@ -0,0 +1,13 @@
+package model
+
+import org.scalatest.{FunSuite}
+
+import com.yen.RedditScrapper.model.Schema
+
+class SchemaSuite extends FunSuite{
+
+  val mySchema = new Schema()
+
+  assert (mySchema.commentFilterCols == List("body", "id", "score", "author", "author_fullname", "parent_id", "created_utc"))
+  assert (mySchema.submissionFilterCols == List("title", "selftext", "id", "upvote_ratio", "num_comments", "link_flair_text", "score", "created_utc", "author", "author_fullname", "retrieved_on"))
+}
